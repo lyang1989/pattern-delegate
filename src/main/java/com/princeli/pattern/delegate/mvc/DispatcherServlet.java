be93626f5cacc88df78fb5c24bc1ffee8a138b45
lyang1989 <lyang1989@gmail.com>
2019-09-02 22:01:16 +0800

    优化委派模式

--- src/main/java/com/princeli/pattern/delegate/mvc/DispatcherServlet.java
+++ src/main/java/com/princeli/pattern/delegate/mvc/DispatcherServlet.java
@@ -1,14 +1,16 @@
 package com.princeli.pattern.delegate.mvc;
 
 import com.princeli.pattern.delegate.mvc.controller.MemberController;
-import com.princeli.pattern.delegate.mvc.controller.OrderController;
-import com.princeli.pattern.delegate.mvc.controller.SystemController;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : princeli
@@ -18,6 +20,30 @@ import java.io.IOException;
  * @description: TODO
  */
 public class DispatcherServlet extends HttpServlet {
+
+    private List<Handler> handlerMapping = new ArrayList<Handler>();
+
+
+    @Override
+    public void init() throws ServletException {
+        Class<?> memberControllerClass = MemberController.class;
+        try {
+            handlerMapping.add(new Handler().setController(memberControllerClass.newInstance())
+                    .setMethod(memberControllerClass.getMethod("getMemberById",new Class[]{String.class}))
+                    .setUrl("/web/getMemberById.json")
+
+            );
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.service(req, resp);
@@ -25,20 +51,79 @@ public class DispatcherServlet extends HttpServlet {
         doDispatch(req,resp);
     }
 
-    private void doDispatch(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    private void doDispatch(HttpServletRequest req, HttpServletResponse resp) {
         String uri = req.getRequestURI();
-        String mid = req.getParameter("mid");
-
-
-        if ("getMemberById".equals(uri)){
-            new MemberController().getMemberById(mid);
-        } else if ("getOrderById".equals(uri)){
-            new OrderController().getOrderById(mid);
-        } else if ("logout".equals(uri)){
-            new SystemController().logout(mid);
-        }else {
-            resp.getWriter().write("404 Not Found !");
+        Handler handler = null;
+        for (Handler h:handlerMapping) {
+            if (uri.equals(h.getUrl())){
+                handler = h;
+                break;
+            }
         }
 
+        Object obj = null;
+        try {
+            obj = handler.getMethod().invoke(handler.getController(),req.getParameter("mid"));
+            resp.getWriter().write(obj.toString());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+//    private void doDispatch(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//        String uri = req.getRequestURI();
+//        String mid = req.getParameter("mid");
+//
+//
+//        if ("getMemberById".equals(uri)){
+//            new MemberController().getMemberById(mid);
+//        } else if ("getOrderById".equals(uri)){
+//            new OrderController().getOrderById(mid);
+//        } else if ("logout".equals(uri)){
+//            new SystemController().logout(mid);
+//        }else {
+//            resp.getWriter().write("404 Not Found !");
+//        }
+//
+//    }
+
+
+    class Handler {
+
+        private Object controller;
+        private Method method;
+        private String url;
+
+        public Object getController() {
+            return controller;
+        }
+
+        public Handler setController(Object controller) {
+            this.controller = controller;
+            return this;
+        }
+
+        public Method getMethod() {
+            return method;
+        }
+
+        public Handler setMethod(Method method) {
+            this.method = method;
+            return this;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public Handler setUrl(String url) {
+            this.url = url;
+            return this;
+        }
     }
 }
